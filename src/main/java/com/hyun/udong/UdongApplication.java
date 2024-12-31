5ee3867b0478af44b21460a28e1db5418a6d204b
hyn <qqqq_o@naver.com>
2025-01-01 02:41:36 +0900

    fix: 시큐리티 기본 로그인 화면 표시되지 않게 수정

--- src/main/java/com/hyun/udong/UdongApplication.java
+++ src/main/java/com/hyun/udong/UdongApplication.java
@@ -2,12 +2,13 @@ package com.hyun.udong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UdongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UdongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UdongApplication.class, args);
+    }
 
 }
