6aff17e0c292c7a6d35ff3bd6f2a69affc590186
hyn <qqqq_o@naver.com>
2025-01-03 23:50:07 +0900

    refactor: KakaoTokenResponse에 JsonNaming (sname->camel) 적용

--- src/main/java/com/hyun/udong/auth/presentation/dto/KakaoTokenResponse.java
+++ src/main/java/com/hyun/udong/auth/presentation/dto/KakaoTokenResponse.java
@@ -1,7 +1,8 @@
 package com.hyun.udong.auth.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,24 +10,19 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoTokenResponse {
 
-    @JsonProperty("token_type")
     private String tokenType;
 
-    @JsonProperty("access_token")
     private String accessToken;
 
-    @JsonProperty("id_token")
     private String idToken;
 
-    @JsonProperty("expires_in")
     private Integer expiresIn;
 
-    @JsonProperty("refresh_token")
     private String refreshToken;
 
-    @JsonProperty("refresh_token_expires_in")
     private Integer refreshTokenExpiresIn;
 
     private String scope;
