1956c1aba748448919bbebb5577190396fff1a35
hyn <qqqq_o@naver.com>
2025-01-03 23:16:55 +0900

    refactor: AuthService 추가 후 카카오 로그인 로직 이동

---
+++ src/main/java/com/hyun/udong/auth/application/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.hyun.udong.auth.application.service;
+
+import com.hyun.udong.auth.infrastructure.client.KakaoOAuthClient;
+import com.hyun.udong.auth.presentation.dto.AccessTokenResponse;
+import com.hyun.udong.auth.presentation.dto.KakaoProfileResponse;
+import com.hyun.udong.auth.presentation.dto.KakaoTokenResponse;
+import com.hyun.udong.member.application.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final KakaoOAuthClient kakaoOAuthClient;
+    private final MemberService memberService;
+
+    public AccessTokenResponse kakaoLogin(String code) {
+        KakaoTokenResponse kakaoTokenResponse = kakaoOAuthClient.getToken(code);
+        KakaoProfileResponse profile = kakaoOAuthClient.getUserProfile(kakaoTokenResponse.getAccessToken());
+        memberService.saveOrUpdate(profile.toMember());
+        return new AccessTokenResponse(kakaoTokenResponse.getIdToken(), kakaoTokenResponse.getExpiresIn());
+    }
+}
--- src/main/java/com/hyun/udong/auth/presentation/controller/AuthController.java
+++ src/main/java/com/hyun/udong/auth/presentation/controller/AuthController.java
@@ -1,10 +1,7 @@
 package com.hyun.udong.auth.presentation.controller;
 
-import com.hyun.udong.auth.infrastructure.client.KakaoOAuthClient;
+import com.hyun.udong.auth.application.service.AuthService;
 import com.hyun.udong.auth.presentation.dto.AccessTokenResponse;
-import com.hyun.udong.auth.presentation.dto.KakaoProfileResponse;
-import com.hyun.udong.auth.presentation.dto.KakaoTokenResponse;
-import com.hyun.udong.member.application.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final KakaoOAuthClient kakaoOAuthClient;
-    private final MemberService memberService;
+    private final AuthService authService;
+
 
     @GetMapping("/login/kakao")
     public ResponseEntity<AccessTokenResponse> kakaoLogin(@RequestParam("code") final String code) {
-        KakaoTokenResponse kakaoTokenResponse = kakaoOAuthClient.getToken(code);
-        KakaoProfileResponse profile = kakaoOAuthClient.getUserProfile(kakaoTokenResponse.getAccessToken());
-        memberService.saveOrUpdate(profile.toMember());
-        return ResponseEntity.ok().body(new AccessTokenResponse(kakaoTokenResponse.getIdToken(), kakaoTokenResponse.getExpiresIn()));
+        AccessTokenResponse accessToken = authService.kakaoLogin(code);
+        return ResponseEntity.ok().body(accessToken);
     }
 }
