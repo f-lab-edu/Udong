456123ed4a712a02932296dfe62264c189acc7f7
hyn <qqqq_o@naver.com>
2025-01-03 23:42:38 +0900

    refactor: KakaoProfileResponse 내 타입 내부 클래스로 변경

--- src/main/java/com/hyun/udong/auth/presentation/dto/KakaoProfileResponse.java
+++ src/main/java/com/hyun/udong/auth/presentation/dto/KakaoProfileResponse.java
@@ -1,38 +1,49 @@
 package com.hyun.udong.auth.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hyun.udong.member.domain.Member;
 import com.hyun.udong.member.domain.SocialType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoProfileResponse {
 
-    //회원 번호
-    @JsonProperty("id")
     private Long id;
 
-    private String nickname;
+    private KakaoAccount kakaoAccount;
 
-    private String profileImageUrl;
+    @Getter
+    public static class KakaoAccount {
 
-    @SuppressWarnings("unchecked")
-    @JsonProperty("kakao_account")
-    private void unpackNested(Map<String, Object> kakaoAccount) {
-        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
-        this.nickname = (String) profile.get("nickname");
-        this.profileImageUrl = (String) profile.get("profile_image_url");
+        private Profile profile;
+
+        @Getter
+        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+        private static class Profile {
+
+            private String nickname;
+            private String profileImageUrl;
+        }
+
+        public String getNickname() {
+            return profile.getNickname();
+        }
+
+        public String getProfileImageUrl() {
+            return profile.getProfileImageUrl();
+        }
     }
 
     public Member toMember() {
-        return new Member(id, SocialType.KAKAO, getNickname(), getProfileImageUrl());
+        KakaoAccount account = getKakaoAccount();
+        return new Member(id, SocialType.KAKAO, account.getNickname(), account.getProfileImageUrl());
     }
 }
