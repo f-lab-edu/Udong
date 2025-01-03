24edf926e668dfd2a967da211a9b550fec613ff5
hyn <qqqq_o@naver.com>
2025-01-03 23:18:42 +0900

    refactor: AccessTokenResponse getter 제거 및 record 클래스로 변경

--- src/main/java/com/hyun/udong/auth/presentation/dto/AccessTokenResponse.java
+++ src/main/java/com/hyun/udong/auth/presentation/dto/AccessTokenResponse.java
@@ -1,19 +1,4 @@
 package com.hyun.udong.auth.presentation.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class AccessTokenResponse {
-    private final String accessToken;
-    private final long expiredTime;
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public long getExpiredTime() {
-        return expiredTime;
-    }
+public record AccessTokenResponse(String accessToken, long expiredTime) {
 }
