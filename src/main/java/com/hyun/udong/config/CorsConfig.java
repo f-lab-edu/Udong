5d2c4929ee237d1011a38a712e0e84c4f6a55ab4
hyn <qqqq_o@naver.com>
2025-01-11 00:52:25 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/hyun/udong/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.hyun.udong.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+        allowedOriginPatterns.add("http://localhost:5173");
+
+        String[] patterns = allowedOriginPatterns.toArray(String[]::new);
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns(patterns)
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true);
+    }
+}
