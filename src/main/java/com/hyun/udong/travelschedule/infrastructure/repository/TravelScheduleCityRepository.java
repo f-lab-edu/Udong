8891965d9321d1bdbddc1ad073b1a5a075514fb9
hyn <qqqq_o@naver.com>
2025-01-24 07:59:17 +0900

    refactor: 미사용 repository 제거

--- src/main/java/com/hyun/udong/travelschedule/infrastructure/repository/TravelScheduleCityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hyun.udong.travelschedule.infrastructure.repository;
-
-import com.hyun.udong.travelschedule.domain.TravelScheduleCity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelScheduleCityRepository extends JpaRepository<TravelScheduleCity, Long> {
-}
