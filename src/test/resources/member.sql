354957e477d56b20320349ab492838b0db2bcfab
hyn <qqqq_o@naver.com>
2025-01-29 03:23:35 +0900

    refactor: member.sql 중복 삽입 방지

--- src/test/resources/member.sql
+++ src/test/resources/member.sql
@@ -1,2 +1,3 @@
 INSERT INTO member (member_id, social_id, social_type, nickname, age, profile_image_url)
-VALUES (1, 1, 'KAKAO', 'hyun', 25, 'profile_image');
+VALUES (1, 1, 'KAKAO', 'hyun', 25, 'profile_image')
+ON CONFLICT (member_id) DO NOTHING;
