4fa5360dc5de6900cb4e3fb4fa221da0e0eb6c0f
hyn <qqqq_o@naver.com>
2025-01-31 01:22:02 +0900

    refactor: 사용하지 않는 에러코드 제거

--- src/main/java/com/hyun/udong/common/exception/ErrorCode.java
+++ src/main/java/com/hyun/udong/common/exception/ErrorCode.java
@@ -8,12 +8,9 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
 public enum ErrorCode {
-    MEMBER_NOT_FOUND(BAD_REQUEST, "해당하는 회원이 없습니다."),
     INVALID_TOKEN(UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_EXPIRED(UNAUTHORIZED, "만료된 토큰입니다."),
     UNAUTHENTICATED_MEMBER(UNAUTHORIZED, "인증되지 않은 회원입니다."),
-    CITY_NOT_FOUND(BAD_REQUEST, "해당하는 도시가 없습니다."),
-    MEMBER_TRAVEL_SCHEDULE_NOT_FOUND(BAD_REQUEST, "해당 회원의 여행 일정이 존재하지 않습니다."),
     NOT_FOUND(BAD_REQUEST);
 
     private final HttpStatus status;
--- src/main/java/com/hyun/udong/common/exception/NotFoundException.java
+++ src/main/java/com/hyun/udong/common/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package com.hyun.udong.common.exception;
 
 public class NotFoundException extends UdongException {
     public NotFoundException(String message) {
-        super(ErrorCode.MEMBER_TRAVEL_SCHEDULE_NOT_FOUND, message);
+        super(ErrorCode.NOT_FOUND, message);
     }
 }
