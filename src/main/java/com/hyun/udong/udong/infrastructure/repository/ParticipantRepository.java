b3cc82b9e6a1c190e48e2d08527ecf4eb8364e43
hyn <qqqq_o@naver.com>
2025-02-10 23:26:41 +0900

    refactor: ParticipantRepository 미사용 메서드 제거

--- src/main/java/com/hyun/udong/udong/infrastructure/repository/ParticipantRepository.java
+++ src/main/java/com/hyun/udong/udong/infrastructure/repository/ParticipantRepository.java
@@ -3,8 +3,5 @@ package com.hyun.udong.udong.infrastructure.repository;
 import com.hyun.udong.udong.domain.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-    List<Participant> findAllByUdongId(Long udongId);
 }
