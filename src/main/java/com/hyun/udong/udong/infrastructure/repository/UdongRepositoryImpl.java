322b6029ddb6014ff7e92ec9464f83d785427a51
hyn <qqqq_o@naver.com>
2025-02-15 14:14:48 +0900

    refactor: 우동 조회 쿼리 where 구문 메서드 추출

--- src/main/java/com/hyun/udong/udong/infrastructure/repository/UdongRepositoryImpl.java
+++ src/main/java/com/hyun/udong/udong/infrastructure/repository/UdongRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.hyun.udong.udong.infrastructure.repository;
 import com.hyun.udong.udong.domain.Udong;
 import com.hyun.udong.udong.presentation.dto.request.FindUdongsCondition;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -27,12 +28,7 @@ public class UdongRepositoryImpl implements UdongRepositoryCustom {
                 .selectFrom(udong)
                 .leftJoin(udong.travelCities, travelCity)
                 .fetchJoin()
-                .where(
-                        cityIn(filter.getCities()),
-                        startDateGoe(filter.getStartDate()),
-                        endDateLoe(filter.getEndDate()),
-                        tagsIn(filter.getTags())
-                )
+                .where(createWhere(filter))
                 .orderBy(udong.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -42,15 +38,29 @@ public class UdongRepositoryImpl implements UdongRepositoryCustom {
                 .select(udong.count())
                 .from(udong)
                 .leftJoin(udong.travelCities, travelCity)
-                .where(
-                        cityIn(filter.getCities()),
-                        startDateGoe(filter.getStartDate()),
-                        endDateLoe(filter.getEndDate()),
-                        tagsIn(filter.getTags())
-                )
+                .where(createWhere(filter))
                 .fetchOne()).orElse(0L);
 
         return new PageImpl<>(udongs, pageable, total);
+    }
+
+    private BooleanExpression createWhere(FindUdongsCondition filter) {
+        BooleanExpression condition = Expressions.TRUE;
+
+        if (filter.getCities() != null && !filter.getCities().isEmpty()) {
+            condition = condition.and(cityIn(filter.getCities()));
+        }
+        if (filter.getStartDate() != null) {
+            condition = condition.and(startDateGoe(filter.getStartDate()));
+        }
+        if (filter.getEndDate() != null) {
+            condition = condition.and(endDateLoe(filter.getEndDate()));
+        }
+        if (filter.getTags() != null && !filter.getTags().isEmpty()) {
+            condition = condition.and(tagsIn(filter.getTags()));
+        }
+
+        return condition;
     }
 
     private BooleanExpression cityIn(List<Long> cities) {
