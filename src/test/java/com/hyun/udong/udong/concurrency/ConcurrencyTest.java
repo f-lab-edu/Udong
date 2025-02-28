1b629a45a37ab22d3038477229db01a6c0e57d9f
hyn <qqqq_o@naver.com>
2025-02-28 21:02:37 +0900

    test: 낙관적, 비관적 락 성능 비교 테스트 추가

--- src/test/java/com/hyun/udong/udong/concurrency/ConcurrencyTest.java
+++ src/test/java/com/hyun/udong/udong/concurrency/ConcurrencyTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -19,6 +21,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ConcurrencyTest {
+
+    private static final int BIG_REQUEST = 1000;
+    private static final int BIG_REQUEST_THREADS = Runtime.getRuntime().availableProcessors() * 4;
+    private static final int SMALL_REQUEST = 10;
+    private static final int SMALL_REQUEST_THREADS = 2;
 
     private Udong udong;
 
@@ -48,6 +55,20 @@ class ConcurrencyTest {
         }
         udong.setCurrentWaitingMemberCount(4);
         udongRepository.save(udong);
+    }
+
+    private static void printRespoinseTimes(List<Long> responseTimes) {
+        List<Long> responseTimeMillis = responseTimes.stream()
+                .map(time -> time / 1_000_000)
+                .toList();
+
+        long fastest = responseTimeMillis.stream().min(Long::compare).orElse(0L);
+        long slowest = responseTimeMillis.stream().max(Long::compare).orElse(0L);
+        double average = responseTimeMillis.stream().mapToLong(Long::longValue).average().orElse(0);
+
+        System.out.println("최단 응답 시간: " + fastest + "ms");
+        System.out.println("최장 응답 시간: " + slowest + "ms");
+        System.out.println("평균 응답 시간: " + average + "ms");
     }
 
     @Test
@@ -82,21 +103,27 @@ class ConcurrencyTest {
     }
 
     @Test
-    void 대기자_리스트_초과_동시성_테스트_낙관적락() throws InterruptedException {
-        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_낙관적락");
+    void 대기자_리스트_초과_동시성_테스트_낙관적락_적은_충돌() throws InterruptedException {
+        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
+
         // given
-        final int REQUEST_MEMBER_COUNT = 100;
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4);
+        final int REQUEST_MEMBER_COUNT = SMALL_REQUEST;
+        ExecutorService executorService = Executors.newFixedThreadPool(SMALL_REQUEST_THREADS);
         CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
 
+        long startTime = System.nanoTime();
         // when
         long startMemberId = 5L;
         for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
             long memberId = startMemberId + i;
             executorService.submit(() -> {
+                long requestStartTime = System.nanoTime();
                 try {
                     udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId);
                 } finally {
+                    long requestEndTime = System.nanoTime();
+                    responseTimes.add(requestEndTime - requestStartTime);
+
                     latch.countDown();
                 }
             });
@@ -108,24 +135,79 @@ class ConcurrencyTest {
         // then
         int waitingCount = waitingMemberRepository.countByUdong(udong);
         assertThat(waitingCount).isEqualTo(5);
+
+        // 응답 시간
+        long endTime = System.nanoTime();
+        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
+
+        printRespoinseTimes(responseTimes);
     }
 
     @Test
-    void 대기자_리스트_초과_동시성_테스트_비관적락() throws InterruptedException {
-        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_비관적락");
+    void 대기자_리스트_초과_동시성_테스트_낙관적락_많은_충돌() throws InterruptedException {
+        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
+
         // given
-        final int REQUEST_MEMBER_COUNT = 100;
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4);
+        final int REQUEST_MEMBER_COUNT = BIG_REQUEST;
+        ExecutorService executorService = Executors.newFixedThreadPool(BIG_REQUEST_THREADS);
         CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
+
+        long startTime = System.nanoTime();
+        // when
+        long startMemberId = 5L;
+        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
+            long memberId = startMemberId + i;
+            executorService.submit(() -> {
+                long requestStartTime = System.nanoTime();
+                try {
+                    udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId);
+                } finally {
+                    long requestEndTime = System.nanoTime();
+                    responseTimes.add(requestEndTime - requestStartTime);
+
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(); // 스레드 완료될 때까지 대기
+        executorService.shutdown();
+
+        // then
+        int waitingCount = waitingMemberRepository.countByUdong(udong);
+        assertThat(waitingCount).isEqualTo(5);
+
+        // 응답 시간
+        long endTime = System.nanoTime();
+        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
+
+        printRespoinseTimes(responseTimes);
+    }
+
+    @Test
+    void 대기자_리스트_초과_동시성_테스트_비관적락_적은_충돌() throws InterruptedException {
+        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_비관적락");
+        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
+
+        // given
+        final int REQUEST_MEMBER_COUNT = SMALL_REQUEST;
+        ExecutorService executorService = Executors.newFixedThreadPool(SMALL_REQUEST_THREADS);
+        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
+
+        long startTime = System.nanoTime();
 
         // when
         long startMemberId = 5L;
         for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
             long memberId = startMemberId + i;
             executorService.submit(() -> {
+                long requestStartTime = System.nanoTime();
                 try {
                     udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId);
                 } finally {
+                    long requestEndTime = System.nanoTime();
+                    responseTimes.add((requestEndTime - requestStartTime));
+
                     latch.countDown();
                 }
             });
@@ -137,5 +219,54 @@ class ConcurrencyTest {
         // then
         int waitingCount = waitingMemberRepository.countByUdong(udong);
         assertThat(waitingCount).isEqualTo(5);
+
+        // 응답 시간
+        long endTime = System.nanoTime();
+        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
+
+        printRespoinseTimes(responseTimes);
+    }
+
+    @Test
+    void 대기자_리스트_초과_동시성_테스트_비관적락_많은_충돌() throws InterruptedException {
+        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_비관적락");
+        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
+
+        // given
+        final int REQUEST_MEMBER_COUNT = BIG_REQUEST;
+        ExecutorService executorService = Executors.newFixedThreadPool(BIG_REQUEST_THREADS);
+        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
+
+        long startTime = System.nanoTime();
+
+        // when
+        long startMemberId = 5L;
+        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
+            long memberId = startMemberId + i;
+            executorService.submit(() -> {
+                long requestStartTime = System.nanoTime();
+                try {
+                    udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId);
+                } finally {
+                    long requestEndTime = System.nanoTime();
+                    responseTimes.add((requestEndTime - requestStartTime));
+
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(); // 스레드 완료될 때까지 대기
+        executorService.shutdown();
+
+        // then
+        int waitingCount = waitingMemberRepository.countByUdong(udong);
+        assertThat(waitingCount).isEqualTo(5);
+
+        // 응답 시간
+        long endTime = System.nanoTime();
+        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
+
+        printRespoinseTimes(responseTimes);
     }
 }
