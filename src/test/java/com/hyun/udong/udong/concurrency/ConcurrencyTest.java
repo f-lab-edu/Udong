873a6a1cb51ad03bdd87cc435866066dfe06f487
hyn <qqqq_o@naver.com>
2025-02-28 21:46:17 +0900

    test&refactor: 락 성능 비교 테스트 템플릿 메서드 추가

--- src/test/java/com/hyun/udong/udong/concurrency/ConcurrencyTest.java
+++ src/test/java/com/hyun/udong/udong/concurrency/ConcurrencyTest.java
@@ -22,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class ConcurrencyTest {
 
+    private static final int MAX_WAITING_COUNT = 5;
     private static final int BIG_REQUEST = 1000;
     private static final int BIG_REQUEST_THREADS = Runtime.getRuntime().availableProcessors() * 4;
     private static final int SMALL_REQUEST = 10;
@@ -40,7 +41,6 @@ class ConcurrencyTest {
 
     @BeforeEach
     void setUp() {
-        System.out.println(">>>>>>>>>>>> setUp");
         udong = Udong.builder()
                 .content(Content.of("여행 동행 모집", "즐겁게 여행할 동행을 구합니다."))
                 .recruitPlanner(RecruitPlanner.from(5))
@@ -55,20 +55,6 @@ class ConcurrencyTest {
         }
         udong.setCurrentWaitingMemberCount(4);
         udongRepository.save(udong);
-    }
-
-    private static void printRespoinseTimes(List<Long> responseTimes) {
-        List<Long> responseTimeMillis = responseTimes.stream()
-                .map(time -> time / 1_000_000)
-                .toList();
-
-        long fastest = responseTimeMillis.stream().min(Long::compare).orElse(0L);
-        long slowest = responseTimeMillis.stream().max(Long::compare).orElse(0L);
-        double average = responseTimeMillis.stream().mapToLong(Long::longValue).average().orElse(0);
-
-        System.out.println("최단 응답 시간: " + fastest + "ms");
-        System.out.println("최장 응답 시간: " + slowest + "ms");
-        System.out.println("평균 응답 시간: " + average + "ms");
     }
 
     @Test
@@ -102,24 +88,29 @@ class ConcurrencyTest {
         assertThat(waitingCount).isEqualTo(6); // 5명이 제한인데, 6명이 대기자 리스트에 들어감
     }
 
-    @Test
-    void 대기자_리스트_초과_동시성_테스트_낙관적락_적은_충돌() throws InterruptedException {
+    @FunctionalInterface
+    public interface TestExecutor {
+
+        void execute(long memberId);
+    }
+
+    // 락 성능 테스트 템플릿 메서드
+    void runConcurrentTest(int requestCount, int threadPoolSize, TestExecutor executor) throws InterruptedException {
         List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
 
         // given
-        final int REQUEST_MEMBER_COUNT = SMALL_REQUEST;
-        ExecutorService executorService = Executors.newFixedThreadPool(SMALL_REQUEST_THREADS);
-        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+        CountDownLatch latch = new CountDownLatch(requestCount);
 
         long startTime = System.nanoTime();
         // when
         long startMemberId = 5L;
-        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
+        for (int i = 0; i < requestCount; i++) {
             long memberId = startMemberId + i;
             executorService.submit(() -> {
                 long requestStartTime = System.nanoTime();
                 try {
-                    udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId);
+                    executor.execute(memberId);
                 } finally {
                     long requestEndTime = System.nanoTime();
                     responseTimes.add(requestEndTime - requestStartTime);
@@ -134,139 +125,46 @@ class ConcurrencyTest {
 
         // then
         int waitingCount = waitingMemberRepository.countByUdong(udong);
-        assertThat(waitingCount).isEqualTo(5);
+        assertThat(waitingCount).isEqualTo(MAX_WAITING_COUNT);
 
         // 응답 시간
         long endTime = System.nanoTime();
         System.out.println("총 처리 시간(ms):" + (endTime - startTime));
 
-        printRespoinseTimes(responseTimes);
+        List<Long> responseTimeMillis = responseTimes.stream()
+                .map(time -> time / 1_000_000)
+                .toList();
+
+        long fastest = responseTimeMillis.stream().min(Long::compare).orElse(0L);
+        long slowest = responseTimeMillis.stream().max(Long::compare).orElse(0L);
+        double average = responseTimeMillis.stream().mapToLong(Long::longValue).average().orElse(0);
+
+        System.out.println("최단 응답 시간: " + fastest + "ms");
+        System.out.println("최장 응답 시간: " + slowest + "ms");
+        System.out.println("평균 응답 시간: " + average + "ms");
     }
 
     @Test
-    void 대기자_리스트_초과_동시성_테스트_낙관적락_많은_충돌() throws InterruptedException {
-        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
-
-        // given
-        final int REQUEST_MEMBER_COUNT = BIG_REQUEST;
-        ExecutorService executorService = Executors.newFixedThreadPool(BIG_REQUEST_THREADS);
-        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
-
-        long startTime = System.nanoTime();
-        // when
-        long startMemberId = 5L;
-        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
-            long memberId = startMemberId + i;
-            executorService.submit(() -> {
-                long requestStartTime = System.nanoTime();
-                try {
-                    udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId);
-                } finally {
-                    long requestEndTime = System.nanoTime();
-                    responseTimes.add(requestEndTime - requestStartTime);
-
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await(); // 스레드 완료될 때까지 대기
-        executorService.shutdown();
-
-        // then
-        int waitingCount = waitingMemberRepository.countByUdong(udong);
-        assertThat(waitingCount).isEqualTo(5);
-
-        // 응답 시간
-        long endTime = System.nanoTime();
-        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
-
-        printRespoinseTimes(responseTimes);
+    void 낙관적락_적은_충돌() throws InterruptedException {
+        runConcurrentTest(SMALL_REQUEST, SMALL_REQUEST_THREADS,
+                memberId -> udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId));
     }
 
     @Test
-    void 대기자_리스트_초과_동시성_테스트_비관적락_적은_충돌() throws InterruptedException {
-        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_비관적락");
-        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
-
-        // given
-        final int REQUEST_MEMBER_COUNT = SMALL_REQUEST;
-        ExecutorService executorService = Executors.newFixedThreadPool(SMALL_REQUEST_THREADS);
-        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
-
-        long startTime = System.nanoTime();
-
-        // when
-        long startMemberId = 5L;
-        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
-            long memberId = startMemberId + i;
-            executorService.submit(() -> {
-                long requestStartTime = System.nanoTime();
-                try {
-                    udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId);
-                } finally {
-                    long requestEndTime = System.nanoTime();
-                    responseTimes.add((requestEndTime - requestStartTime));
-
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await(); // 스레드 완료될 때까지 대기
-        executorService.shutdown();
-
-        // then
-        int waitingCount = waitingMemberRepository.countByUdong(udong);
-        assertThat(waitingCount).isEqualTo(5);
-
-        // 응답 시간
-        long endTime = System.nanoTime();
-        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
-
-        printRespoinseTimes(responseTimes);
+    void 낙관적락_많은_충돌() throws InterruptedException {
+        runConcurrentTest(BIG_REQUEST, BIG_REQUEST_THREADS,
+                memberId -> udongService.requestParticipationWithOptimisticLock(udong.getId(), memberId));
     }
 
     @Test
-    void 대기자_리스트_초과_동시성_테스트_비관적락_많은_충돌() throws InterruptedException {
-        System.out.println(">>>>>>>>>>>> ConcurrencyTest.대기자_리스트_초과_동시성_테스트_비관적락");
-        List<Long> responseTimes = Collections.synchronizedList(new ArrayList<>());
+    void 비관적락_적은_충돌() throws InterruptedException {
+        runConcurrentTest(SMALL_REQUEST, SMALL_REQUEST_THREADS,
+                memberId -> udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId));
+    }
 
-        // given
-        final int REQUEST_MEMBER_COUNT = BIG_REQUEST;
-        ExecutorService executorService = Executors.newFixedThreadPool(BIG_REQUEST_THREADS);
-        CountDownLatch latch = new CountDownLatch(REQUEST_MEMBER_COUNT);
-
-        long startTime = System.nanoTime();
-
-        // when
-        long startMemberId = 5L;
-        for (int i = 0; i < REQUEST_MEMBER_COUNT; i++) {
-            long memberId = startMemberId + i;
-            executorService.submit(() -> {
-                long requestStartTime = System.nanoTime();
-                try {
-                    udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId);
-                } finally {
-                    long requestEndTime = System.nanoTime();
-                    responseTimes.add((requestEndTime - requestStartTime));
-
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await(); // 스레드 완료될 때까지 대기
-        executorService.shutdown();
-
-        // then
-        int waitingCount = waitingMemberRepository.countByUdong(udong);
-        assertThat(waitingCount).isEqualTo(5);
-
-        // 응답 시간
-        long endTime = System.nanoTime();
-        System.out.println("총 처리 시간(ms):" + (endTime - startTime));
-
-        printRespoinseTimes(responseTimes);
+    @Test
+    void 비관적락_많은_충돌() throws InterruptedException {
+        runConcurrentTest(BIG_REQUEST, BIG_REQUEST_THREADS,
+                memberId -> udongService.requestParticipationWithPessimisticLock(udong.getId(), memberId));
     }
 }
