c6d5aa18e5d63aeb121742d3ef2fe062e8f0fea0
hyn <qqqq_o@naver.com>
2025-07-07 22:47:51 +0900

    feat: 로그아웃 엔드포인트 추가

--- backend/src/test/java/com/hyun/udong/auth/presentation/controller/AuthControllerTest.java
+++ backend/src/test/java/com/hyun/udong/auth/presentation/controller/AuthControllerTest.java
@@ -1,18 +1,6 @@
 package com.hyun.udong.auth.presentation.controller;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
+import com.hyun.udong.auth.application.service.RefreshTokenService;
 import com.hyun.udong.auth.infrastructure.client.KakaoOAuthClient;
 import com.hyun.udong.auth.presentation.dto.KakaoProfileResponse;
 import com.hyun.udong.auth.presentation.dto.KakaoTokenResponse;
@@ -21,9 +9,20 @@ import com.hyun.udong.common.util.DataCleanerExtension;
 import com.hyun.udong.member.application.service.MemberService;
 import com.hyun.udong.member.domain.Member;
 import com.hyun.udong.member.domain.SocialType;
-
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(DataCleanerExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,6 +37,9 @@ class AuthControllerTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
 
     @LocalServerPort
     private int port;
@@ -92,5 +94,23 @@ class AuthControllerTest {
                 .body("nickname", equalTo(member.getNickname()))
                 .body("token.accessToken", notNullValue())
                 .body("token.refreshToken", notNullValue());
+    }
+
+    @Test
+    void 로그아웃_요청시_204와_블랙리스트_등록이_정상적으로_동작한다() {
+        // given
+        Long memberId = 12345L;
+        String refreshToken = jwtTokenProvider.generateRefreshToken(memberId);
+        refreshTokenService.save(refreshToken, memberId.toString(), jwtTokenProvider.getTokenExpireTime(refreshToken));
+
+        // when & then
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("refreshToken", refreshToken)
+                .when()
+                .post("/api/auth/logout")
+                .then().log().all()
+                .statusCode(204);
     }
 }
